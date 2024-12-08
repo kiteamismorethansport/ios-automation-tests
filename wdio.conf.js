400e783c5d6b2205f28a38c3088ad56dc620174c
Dmytro Kovalov <dmytroidmytroidmytro@gmail.com>
2024-12-08 12:53:45 +0000

    2.1

--- wdio.conf.js
+++ wdio.conf.js
@@ -6,7 +6,7 @@ export const config = {
             'appium:platformVersion': '17.5', // Add appium prefix
             'appium:deviceName': 'iPhone 14', // Add appium prefix
             'appium:automationName': 'XCUITest', // Add appium prefix
-            'appium:app': './apps/BitsBarSampleApp.app', // Add appium prefix
+            'appium:app': './apps/BitBarSampleApp.app', // Add appium prefix
             'appium:noReset': true, // Add appium prefix
             'appium:fullReset': false, // Add appium prefix
         },
