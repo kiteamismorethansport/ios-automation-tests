4b9bb76f19d110952e8094e439a0c4b1b4e4ea82
Dmytro Kovalov <dmytroidmytroidmytro@gmail.com>
2024-12-08 11:47:33 +0000

    1.2

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,20 +1,34 @@
-exports.config = {
-    runner: 'local',
-    framework: 'mocha',
-    capabilities: [
-      {
-        platformName: 'iOS', // Testing platform
-    platformVersion: '18.1', // The iOS version for the simulator
-    deviceName: 'iPhone 15', // Name of the simulator device
-    automationName: 'XCUITest', // Appium's automation driver for iOS
-        app: '/app/wdiodemoapp.app/',
-        noReset: true,
-        fullReset: false,
-      },
-    ],
+import { config as baseConfig } from '@wdio/sync';
+
+export const config = {
+    // Base URL for the WebdriverIO tests
     baseUrl: 'http://127.0.0.1:4723',
+
+    // Capabilities
+    capabilities: [
+        {
+          platformName: 'iOS', // Testing platform
+      platformVersion: '18.1', // The iOS version for the simulator
+      deviceName: 'iPhone 15', // Name of the simulator device
+      automationName: 'XCUITest', // Appium's automation driver for iOS
+          app: '/app/BitBarSampleApp.app/',
+          noReset: true,
+          fullReset: false,
+        },
+  
+    ],
+
+    // Framework
+    framework: 'mocha',
+
+    // Test Files
+    specs: ['./tests/specs/**/*.js'],
+
+    // Services
     services: ['appium'],
+
+    // Mocha Options
     mochaOpts: {
-      timeout: 60000,
+        timeout: 60000,
     },
-  };
+};
