d45e586b0ab56f093d473f4febcc7fbef2582621
Dmytro Kovalov <dmytroidmytroidmytro@gmail.com>
2024-12-08 12:01:30 +0000

    1.4

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,21 +1,15 @@
-import { config as baseConfig } from '@wdio/sync';
-
 export const config = {
-    // Base URL for the WebdriverIO tests
-    baseUrl: 'http://127.0.0.1:4723',
-
     // Capabilities
     capabilities: [
         {
-          platformName: 'iOS', // Testing platform
-      platformVersion: '18.1', // The iOS version for the simulator
-      deviceName: 'iPhone 15', // Name of the simulator device
-      automationName: 'XCUITest', // Appium's automation driver for iOS
-          app: '/app/BitBarSampleApp.app/',
-          noReset: true,
-          fullReset: false,
+            platformName: 'iOS',
+            platformVersion: '16.0',
+            deviceName: 'iPhone 14',
+            automationName: 'XCUITest',
+            app: './apps/YourApp.app',
+            noReset: true,
+            fullReset: false,
         },
-  
     ],
 
     // Framework
