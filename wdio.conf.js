84a7fcda9eae71de0ccd670b8925b988756e3185
Dmytro Kovalov <dmytroidmytroidmytro@gmail.com>
2024-12-17 22:37:20 +0000

    4.0

--- wdio.conf.js
+++ wdio.conf.js
@@ -8,7 +8,7 @@ export const config = {
             'appium:automationName': 'XCUITest', // Add appium prefix
             'appium:app': './apps/MyRNDemoApp.app', // Add appium prefix
             'appium:noReset': true, // Add appium prefix
-            'appium:fullReset': false, // Add appium prefix
+            'appium:fullReset': false, // Add appium prefix!
         },
     ],
 
