c465916e357bfe4d0923680bbbd9510a732a3857
Dmytro Kovalov <dmytroidmytroidmytro@gmail.com>
2024-12-17 20:44:07 +0000

    3.0

--- wdio.conf.js
+++ wdio.conf.js
@@ -6,7 +6,7 @@ export const config = {
             'appium:platformVersion': '17.5', // Add appium prefix
             'appium:deviceName': 'iPhone 15', // Add appium prefix
             'appium:automationName': 'XCUITest', // Add appium prefix
-            'appium:app': './apps/MyRNDemoApp.app', // Add appium prefix
+            'appium:app': './apps/MyRNDemoApp', // Add appium prefix
             'appium:noReset': true, // Add appium prefix
             'appium:fullReset': false, // Add appium prefix
         },
