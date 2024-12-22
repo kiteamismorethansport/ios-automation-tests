0c81b35854e42be051139327e566b24333d019fc
Dmytro Kovalov <dmytroidmytroidmytro@gmail.com>
2024-12-22 00:48:35 +0000

    master

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,11 +1,22 @@
- import { expect } from '@wdio/globals'
-import LoginPage from '../pageobjects/login.page.js'
+import fs from 'fs';
+import { expect } from '@wdio/globals';
+import LoginPage from '../pageobjects/login.page.js';
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
-    await browser.saveScreenshot('./screenshots/before-click.png');
-    await LoginPage.click()
-    await browser.saveScreenshot('./screenshots/after-click.png');
-    })
-})
+        const screenshotsDir = `${process.cwd()}/screenshots`;
+        if (!fs.existsSync(screenshotsDir)) {
+            fs.mkdirSync(screenshotsDir); // Ensure directory exists
+        }
+
+        console.log('Saving screenshot before click');
+        await browser.saveScreenshot(`${screenshotsDir}/before-click.png`);
+
+        console.log('Performing click action');
+        await LoginPage.click();
+
+        console.log('Saving screenshot after click');
+        await browser.saveScreenshot(`${screenshotsDir}/after-click.png`);
+    });
+});
 
