568262e2362ed4e3e856a85245f1487322c2234e
Dmytro Kovalov <dmytroidmytroidmytro@gmail.com>
2024-12-30 17:16:03 +0000

    1

--- wdio.conf.js
+++ wdio.conf.js
@@ -34,7 +34,7 @@ export const config = {
       {
         platformName: 'iOS',
         'appium:platformVersion': '18.1',
-        'appium:deviceName': 'iPad (9th generation)',
+        'appium:deviceName': '55AC4BC1-FCCB-4CC4-B813-C1C82C46506D',
         'appium:automationName': 'XCUITest',
         'appium:app': './apps/MyRNDemoApp.app',
   
